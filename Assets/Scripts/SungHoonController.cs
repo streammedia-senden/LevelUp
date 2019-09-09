b9c0be6aa5929027d3bd658ec774bd232bfb846c
streammedia-senden <55079639+streammedia-senden@users.noreply.github.com>
2019-09-09 17:42:37 +0900

    initial commit.

---
+++ Assets/Scripts/SungHoonController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SungHoonController : MonoBehaviour {
+
+    private Rigidbody2D myRigidBody;
+    public float SungHoonJumpForce = 500f;
+
+    // Use this for initialization
+    void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButtonUp("Jump"))
+        {
+            myRigidBody.AddForce(transform.up * SungHoonJumpForce);
+        }
+     }
+}
